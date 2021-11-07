4beeca3576bf4c47094c6d80c9c1540402955270
Primo-Victoria <91014253+Primo-Victoria@users.noreply.github.com>
2021-11-07 17:54:05 +0300

    Добавил профиль

--- src/router/index.js
+++ src/router/index.js
@@ -42,7 +42,20 @@ export const constantRoutes = [
     component: () => import('@/views/404'),
     hidden: true
   },
-
+  {
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    hidden: true,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/profile/index'),
+        name: 'Profile',
+        meta: { title: 'Profile', icon: 'user', noCache: true }
+      }
+    ]
+  },
   {
     path: '/',
     component: Layout,
@@ -170,7 +183,7 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
